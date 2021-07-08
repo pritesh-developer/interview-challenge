ab11019349e02da5f0dd863ce6dafee5eca87ece
pritesh paul <paul.pritesh@gmail.com>
2021-07-08 13:37:58 +0530

    Added dietry type count and menu preview home

--- src/client/pages/Home/HomePage.js
+++ src/client/pages/Home/HomePage.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { connect } from 'react-redux';
+import { Menu } from '../../components/Menu/Menu';
+import { MenuPreview} from '../../components/MenuPreview/MenuPreview';
 
 const Home = (menus) => {
-
+   const [initState, setState] = useState(null);
+   const handleGetSelectedMenu =useCallback((menu) => {
+       console.log("called",menu)
+      setState(menu);
+   },[]);
+   console.log(menus,"menuPreviews")
     return (
         <div className="wrapper">
             <div className="menu-summary">
                 <div className="container">
                     <div className="row">
                         <div className="col-6 menu-summary-left">
-                            <span> items</span>
+                            <span>{menus.totalSelected} items</span>
                         </div>
                         <div className="col-6 menu-summary-right">
-                        <Menu menuList={menus} getSelectedMenu={(menu)=> handleGetSelectedMenu(menu)}></Menu>
+                        {menus.newCountDietryUI?.ve} <span className="dietary">ve</span>
+            {menus.newCountDietryUI?.v} <span className="dietary">v</span>
+            {menus.newCountDietryUI?.n} <span className="dietary">n!</span>
+            {menus.newCountDietryUI?.df} <span className="dietary">df</span>
+            {menus.newCountDietryUI?.gf} <span className="dietary">gf</span>
                         </div>
                     </div>
                 </div>
@@ -19,11 +31,11 @@ const Home = (menus) => {
             <div className="container menu-builder">
                 <div className="row">
                     <div className="col-4">
-                       
+                        <Menu menuList={menus} getSelectedMenu={(menu)=> handleGetSelectedMenu(menu)}></Menu>
                     </div>
                     <div className="col-8">
                         <h2>Menu preview</h2>
-                        
+                        <MenuPreview previews={menus.menuPreviews}></MenuPreview>
                         {/* <ul className="menu-preview">
                             <li className="item">
                                 <h2>Dummy item</h2>
@@ -68,3 +80,13 @@ const Home = (menus) => {
         </div>
     )
 }
+
+const mapStateToProps = (state) => ({
+    menus:state.menus,
+    menuPreviews:state.menuPreviews,
+    totalSelected: state.count,
+    newCountDietryUI: state.newCountDietry
+})
+
+const HomePage = connect(mapStateToProps,null)(Home)
+export default HomePage;
